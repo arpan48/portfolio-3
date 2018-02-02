e76752796611aaeb74dd5b0a81554880a90f0dec
Arpan SIngh <arpansingh48@gmail.com>
2018-02-02 15:12:14 +0100

    no images

--- custom-js/nav-side.js
+++ custom-js/nav-side.js
@@ -1,6 +1,6 @@
 /* Set the width of the side navigation to 250px */
 function openNav() {
-    document.getElementById("mySidenav").style.width = "25%";
+    document.getElementById("mySidenav").style.width = "100%";
 }
 
 /* Set the width of the side navigation to 0 */
